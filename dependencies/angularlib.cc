0b552dc7d58650155adbccedac39003a6ea13f6c
Empty-Engineering <lukasmrobin@gmail.com>
2021-11-07 00:25:45 -0400

    33

--- dependencies/angularlib.cc
+++ dependencies/angularlib.cc
@@ -1,6 +1,16 @@
+//Lukas Robin
+//Telemetree
+//Last updated 11.06.2021
+// v0.2.0b
+
+//define constant of encoder resolution
 #define ENC_COUNT_REV 600
+
+//encoder input pins
 #define ENC_IN_RIGHT_A 2
 #define ENC_IN_RIGHT_B 4
+
+
 //define the data bands up to 7 bits
 #define DATA1 22
 #define DATA2 24
@@ -9,30 +19,31 @@
 #define DATA5 30
 #define DATA6 32
 #define DATA7 34
- 
+//sign of vector
+#define DATA8 36
+
+
+
 // True = Forward; False = Reverse
 bool Direction_right = true;
- 
 // Keep track of the binary_rpm of right wheel pulses
 volatile long right_wheel_pulse_count = 0;
- 
-// One-second interval for measurements
-int interval = 1000;
-  
+// half interval for measurements
+int interval = 500;
 // Counters for milliseconds during interval
 long previousMillis = 0;
 long currentMillis = 0;
- 
 // Variable for RPM measurement
 float rpm_right = 0;
 float* rpm_right_ptr = &rpm_right;
- 
 // Variable for angular velocity measurement
 float ang_velocity_right = 0;
 float ang_velocity_right_deg = 0;
- 
  //variable for binary rpms
 int binary_rpm;
+
+
+
 void setup() {
   // Set pin states of the encoder
     pinMode(ENC_IN_RIGHT_A , INPUT_PULLUP);
@@ -59,96 +70,45 @@ void loop() {
     *rpm_right_ptr = sqrt(*rpm_right_ptr);
     long binary_rpm_i = decimalToBinary(*rpm_right_ptr);
     long* binary_rpm = &binary_rpm_i;
-    int n1,n2,n3,n4,n5,n6,n7;
-    n1 = *binary_rpm%10;
-    *binary_rpm /= 10;
-    n2 = *binary_rpm%10;
-    *binary_rpm /= 10;
-    n3 = *binary_rpm%10;
-    *binary_rpm /= 10;
-    n4 = *binary_rpm%10;
-    *binary_rpm /=10;
-    n5 = *binary_rpm%10;
-    *binary_rpm /= 10;
-    n6 = *binary_rpm%10;
-    *binary_rpm /= 10;
+    int n1,n2,n3,n4,n5,n6,n7,n8;
+    n1 = *binary_rpm%10;*binary_rpm /= 10;
+    n2 = *binary_rpm%10;*binary_rpm /= 10;
+    n3 = *binary_rpm%10;*binary_rpm /= 10;
+    n4 = *binary_rpm%10;*binary_rpm /=10;
+    n5 = *binary_rpm%10;*binary_rpm /= 10;
+    n6 = *binary_rpm%10;*binary_rpm /= 10;
     n7 = *binary_rpm%10;
     //output binary over GPIO
-    if (n1){
-        digitalWrite(DATA1, HIGH);
-        }
-        else{
-        digitalWrite(DATA1, LOW);
-        }
-    if (n2){
-        digitalWrite(DATA2, HIGH);
-        }
-        else{
-        digitalWrite(DATA2, LOW);
-        }
-    if (n3){
-        digitalWrite(DATA3, HIGH);
-        }
-        else{
-        digitalWrite(DATA3, LOW);
-        }
-    if (n4){
-        digitalWrite(DATA4, HIGH);
-        }
-        else{
-        digitalWrite(DATA4, LOW);
-        }
-    if (n5){
-        digitalWrite(DATA5, HIGH);
-        }
-        else{
-        digitalWrite(DATA5, LOW);
-        }
-    if (n6){
-        digitalWrite(DATA6, HIGH);
-        }
-        else{
-        digitalWrite(DATA6, LOW);
-        }
-    if (n7){
-        digitalWrite(DATA7, HIGH);
-        }
-        else{
-        digitalWrite(DATA7, LOW);
-        }
+    if (n1){digitalWrite(DATA1, HIGH);}else{digitalWrite(DATA1, LOW);}
+    if (n2){digitalWrite(DATA2, HIGH);}else{digitalWrite(DATA2, LOW);}
+    if (n3){digitalWrite(DATA3, HIGH);}else{digitalWrite(DATA3, LOW);}
+    if (n4){digitalWrite(DATA4, HIGH);}else{digitalWrite(DATA4, LOW);}
+    if (n5){digitalWrite(DATA5, HIGH);}else{digitalWrite(DATA5, LOW);}
+    if (n6){digitalWrite(DATA6, HIGH);}else{digitalWrite(DATA6, LOW);}
+    if (n7){digitalWrite(DATA7, HIGH);}else{digitalWrite(DATA7, LOW);}
     }
     // Reset the pulse count
-    right_wheel_pulse_count = 0;
-   
-  }
+  right_wheel_pulse_count = 0;
+}
 
 long decimalToBinary(long n) {
-    int remainder; 
+    int remainder, m;
     long binary = 0, i = 1;
-   
     while(n != 0) {
         remainder = n%2;
         n = n/2;
-        binary= binary + (remainder*i);
+        binary = binary + (remainder*i);
         i = i*10;
     }
-    return binary;
+  return binary;
 }
  
 // Increment the binary_rpm of pulses by 1
 void right_wheel_pulse() {
   // Read the value for the encoder for the right wheel
   int val = digitalRead(ENC_IN_RIGHT_B);
-  if (!val) {
-    Direction_right = false; // Reverse
-  }
-  else {
-    Direction_right = true; // Forward
-  }
-  if (Direction_right) {
-    right_wheel_pulse_count++;
-  }
-  else {
-    right_wheel_pulse_count--;
-  }
+  if (!val){
+    Direction_right = false;}else{Direction_right = true;}
+  if (Direction_right) {right_wheel_pulse_count++;
+  }else {right_wheel_pulse_count--;}
 }
--- source/Telemetree.py
+++ source/Telemetree.py
@@ -1,51 +1,47 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
-def setup():
-    GPIO.setmode(GPIO.board)
-    GPIO.setup(11, GPIO.IN)
-    GPIO.setup(13, GPIO.IN)
-    GPIO.setup(15, GPIO.IN)
-    GPIO.setup(16, GPIO.IN)
-    GPIO.setup(18, GPIO.IN)
-    GPIO.setup(22, GPIO.IN)
-    GPIO.setup(7, GPIO.IN)
-    GPIO.setup(3, GPIO.IN)
-    GPIO.setup(5, GPIO.IN)
-    GPIO.setup(12, GPIO.IN)
-    GPIO.setup(24, GPIO.IN)
-    GPIO.setup(26, GPIO.IN)
-    GPIO.setup(19, GPIO.IN)
-    GPIO.setup(21, GPIO.IN)
-    GPIO.setup(23, GPIO.IN)
+class data_set:
+    def __init__(self, wheel_radius:int, angular_velocity: int=0, linear_velocity: int=0, power_draw: int=0, exhaust_temperatures: int=0, motor_temperature: int=0, tyre_temperature: int=0, percentage_loss_to_thermal: int=0) -> None:
+        self.wheel_radius = wheel_radius;
+        self.angular_velocity = angular_velocity;
+        self.linear_velocity = linear_velocity;
+        self.power_draw = power_draw;
+        self.exhaust_temperatures = exhaust_temperatures;
+        self.motor_temperature = motor_temperature;
+        self.tyre_temperature = tyre_temperature;
+        self.percentage_loss_to_thermal = percentage_loss_to_thermal;
     
-def data(binary_speed):
-    binary_speed.append(int(GPIO.input(11)))
-    binary_speed.append(int(GPIO.input(13)))
-    binary_speed.append(int(GPIO.input(15)))
-    binary_speed.append(int(GPIO.input(16)))
-    binary_speed.append(int(GPIO.input(18)))
-    binary_speed.append(int(GPIO.input(22)))
-    binary_speed.append(int(GPIO.input(7)))
-    binary_speed.append(int(GPIO.input(3)))
-    binary_speed.append(int(GPIO.input(5)))
-    binary_speed.append(int(GPIO.input(12)))
-    binary_speed.append(int(GPIO.input(24)))
-    binary_speed.append(int(GPIO.input(26)))
-    binary_speed.append(int(GPIO.input(19)))
-    binary_speed.append(int(GPIO.input(21)))
-    binary_speed.append(int(GPIO.input(23)))
-def get_angular_velocity():
-    while (1):
-        binary_speed = []
-        count = 0
-        while (not count):
-            data(binary_speed)
-            count += 1
-        bin = ""
-        for i in binary_speed:
-            bin = str(i)+bin
-            binary_vel = int(bin, 2)
-        return binary_vel
-get_angular_velocity()
+def setup() -> None:
+    GPIO.setmode(GPIO.board);
+    #Angular Velocity
+    GPIO.setup(11, GPIO.IN);
+    GPIO.setup(13, GPIO.IN);
+    GPIO.setup(15, GPIO.IN);
+    GPIO.setup(16, GPIO.IN);
+    GPIO.setup(18, GPIO.IN);
+    GPIO.setup(22, GPIO.IN);
+    GPIO.setup(7, GPIO.IN);
 
-        
+    
+def speed_data(binary_speed) -> None:
+    binary_speed.append(int(GPIO.input(11)));
+    binary_speed.append(int(GPIO.input(13)));
+    binary_speed.append(int(GPIO.input(15)));
+    binary_speed.append(int(GPIO.input(16)));
+    binary_speed.append(int(GPIO.input(18)));
+    binary_speed.append(int(GPIO.input(22)));
+    binary_speed.append(int(GPIO.input(7)));
+
+
+#Uses inputs from GPIO to convert the binary data to an integer value from the angular velocity
+def get_angular_velocity() -> int:
+    binary_speed = [];
+    count = 0;
+    while (not count):
+        speed_data(binary_speed);
+        count += 1;
+    bin = "";
+    for i in binary_speed:
+        bin = str(i)+bin;
+        binary_vel = int(bin, 2);
+    return binary_vel**2;
